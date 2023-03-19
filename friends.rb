438abab811ed714755d64c9f0c71a978b4303448
Scott Baitz <scott.w.baitz@gmail.com>
2023-03-19 08:37:11 -0600

    Add an emobands array with an include method

--- friends.rb
+++ friends.rb
@@ -2,3 +2,6 @@ arrays = ["John", "Jack", "Sue", "Susan", "Krysten"]
 p arrays[2]
 p arrays[4]
 p arrays[0]
+
+emobands = ["snowing", "mineral", "american football", "jawbreaker", "on the might of princes"]
+p emobands.include?("snowing")
